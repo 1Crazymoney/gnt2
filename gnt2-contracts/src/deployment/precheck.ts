742ba120778b8e3e657c3774fe1d2c3a67a02c8f
rzadp <przemek@ethworks.io>
2020-03-13 11:21:01 +0100

    Improve precheck script

--- gnt2-contracts/src/deployment/precheck.ts
+++ gnt2-contracts/src/deployment/precheck.ts
@@ -23,15 +23,38 @@ async function main() {
   const multisig = await new MultiSigWalletFactory(deployer).attach(process.env.multisigAddress!);
   const migrationAgent = await new GNTMigrationAgentFactory(deployer).attach(process.env.GNTMigrationAgentAddress!);
 
+
+  // - The owner of `GNTMigrationAgent` is `golemMigrationMaster`
   const ownerOfMigrationAgent = await migrationAgent.owner();
   if (ownerOfMigrationAgent.toLowerCase() === multisig.address.toLowerCase()) {
     console.log('✅ The owner of migration agent is the multisig (golem migration master)');
   } else throw new Error('The owner of migration agent is incorrect');
 
+
+  // - The target of `GNTMigrationAgent` is `NGNT`
   const target = await migrationAgent.target();
   if (target.toLowerCase() === NGNT.address.toLowerCase()) {
     console.log('✅ The target of migration agent is the NGNT');
   } else throw new Error('The target of migration agent is incorrect');
+
+
+  // - The owner of `GNTMigrationAgent` is the multisig
+  const owner = await migrationAgent.owner();
+  if (owner.toLowerCase() === multisig.address.toLowerCase()) {
+    console.log('✅ The owner of migration agent is multisig');
+  } else throw new Error('The owner of migration agent is incorrect');
+
+
+  // - The **only** minter of `NGNT` is `GNTMigrationAgent`
+  const logsMinterAdded = await provider.getLogs({
+    fromBlock: 1,
+    address: NGNT.address,
+    topics: NGNT.filters.MinterAdded(null).topics
+  });
+  // 2 events - constructor minter and GNTMigrationAgent
+  if (logsMinterAdded.length === 2 && await NGNT.isMinter(migrationAgent.address)) {
+    console.log('✅ The only minter of NGNT is GNTMigrationAgent');
+  } else throw new Error('Incorrect minters of NGNT');
 }
 
 main().catch(reason => {
